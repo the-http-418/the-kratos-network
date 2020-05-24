249ea473e076db769ae525ff6e4638fdf6b99638
avinash-vk <avinash2000vk@gmail.com>
2020-05-24 13:29:43 +0530

    added profile routes

--- frontend/src/components/profile/ProfileList.js
+++ frontend/src/components/profile/ProfileList.js
@@ -1,9 +1,44 @@
-import React from 'react'
+import React,{Component} from 'react'
 
-export default function ProfileList() {
+class ProfileList extends Component {
+    state = {
+        profiles : [],
+    }
+    componentDidMount(){
+        //firebase call
+        this.setState({
+            profiles : [
+                {'firstName':'Patrick','lastName':'Rashidi','heading':'zaio developer','id':'DSDJNDO3904','profilePicture':'/img/default_dp.jpg'},
+            ]
+        })
+    }
+
+    render(){
     return (
         <div>
-            list of profiles here ...
-        </div>
+            <h5>Your Profiles</h5>
+            <hr/>
+            <ul class="collection">
+                {
+                    this.state.profiles.map((profile) => {return(
+                        <li class="collection-item avatar">
+                        <img src={profile['profilePicture']} alt="" class="circle"/>
+                        <span class="title">{profile['firstName']} {profile['lastName']}</span>
+                        <p> {profile['heading']}
+                        </p>
+                        
+                        <a href={`/editprofile/${profile['firstName']}/${profile['id']}`} className="secondary-content material-icons">edit</a>
+                        <a href={`/profile/${profile['firstName']}/${profile['id']}`} className="content material-icons">dvr</a>
+                        
+                    </li>
+                    )})
+                    
+                }
+            </ul>
+    </div>
     )
+    }
 }
+
+
+export default ProfileList
