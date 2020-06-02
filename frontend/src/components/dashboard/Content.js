2b06c17c90e6740b0e293274595abaf3a756c191
avinash-vk <avinash2000vk@gmail.com>
2020-06-03 00:08:01 +0530

    r1 added content pane

---
+++ frontend/src/components/dashboard/Content.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+import Sidenav from '../layout/Sidenav'
+class Content extends Component {
+    constructor(){
+        super()
+        
+    }
+    render() {
+        
+        return (
+            <Sidenav/>
+        )
+    }
+}
+
+export default Content
---
+++ frontend/src/components/layout/Sidenav.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function Sidenav() {
+    return (
+        <div>
+            <ul id="slide-out" class="sidenav sidenav-fixed">
+            <li><a href="#!">Welcome!</a></li>
+            <li><a href="#!">Introduction</a></li>
+        </ul>
+   
+        </div>
+    )
+}
