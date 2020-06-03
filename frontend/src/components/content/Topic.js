af4fd6d5e52256db983e2e9a2e5a6a49c77949a2
avinash-vk <avinash2000vk@gmail.com>
2020-06-03 20:24:15 +0530

    r1 added kebab list xD

--- frontend/src/components/content/Topic.js
+++ frontend/src/components/content/Topic.js
@@ -1,11 +1,29 @@
 import React, { Component } from 'react'
-
+import M from 'materialize-css'
 export default class Topic extends Component {
+    constructor(){
+        super()
+        document.addEventListener('DOMContentLoaded', function() {
+            var elems = document.querySelectorAll('.dropdown-trigger');
+            var instances = M.Dropdown.init(elems);
+          });
+    }
     render() {
         return (
             <div className = "topic-hah">
                 <div className = "topic-title">
-                    <h4>Topic Title</h4>
+                    <div className = "wrapper">
+                    <h4>Topic Title
+                    
+                    <a className='dropdown-trigger right' href='#' data-target='dropdown1'><i className="material-icons small purple-text">more_vert</i></a>
+                    </h4>
+                    
+                    <ul id='dropdown1' className='dropdown-content'>
+                        <li><a href="#!" className='purple-text'>edit<span> <i className="material-icons secondary-content tiny purple-text">edit</i></span></a></li>
+                        <li><a href="#!" className='purple-text'>delete<span><i className="material-icons secondary-content tiny purple-text">delete</i></span></a></li>
+                    </ul>    
+                    
+                </div>
                     <div className = "divider purple darken-3" />
                 </div>
                 <div className = "topic-item-list">
