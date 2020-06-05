251977e82831fc1315d50129b74cc8833d09a0d2
Srujan Deshpande <srujan.deshpande@gmail.com>
2020-06-05 23:02:49 +0530

    r1 semi-working responsive navbar

--- frontend/src/components/layout/Navbar.js
+++ frontend/src/components/layout/Navbar.js
@@ -3,6 +3,8 @@ import {Link} from 'react-router-dom'
 import SignedInLinks from './SignedInLinks'
 import SignedOutLinks from './SignedOutLinks'
 import Fireapp from '../../config/firebaseConfig'
+import M from 'materialize-css'
+import {NavLink} from 'react-router-dom'
 
 class Navbar extends Component {
 
@@ -25,15 +27,25 @@ class Navbar extends Component {
         })
     }
     render(){
+      document.addEventListener('DOMContentLoaded', function() {
+        var elems = document.querySelectorAll('.sidenav');
+        var instances = M.Sidenav.init(elems);
+      });
     console.log("from heret",this.state.links)
     return (
         <div className="navbar-fixed">
         <nav className="nav-wrapper purple darken-3 ">
-            <div className="container">
-                <Link to = '/' className="brand-logo">The Kratos Network</Link>
-                { this.state.links }
-            </div>
+        <a href="#" data-target="mobile-demo" className="sidenav-trigger"><i className="material-icons">menu</i></a>
+          <div className="container">
+            <Link to = '/' className="brand-logo">The Kratos Network</Link>
+              { this.state.links }
+          </div>
         </nav>
+        <ul className="sidenav" id="mobile-demo">
+            <li><NavLink to='/createprofile'>Create Profile</NavLink></li>
+            <li><NavLink to='/profiles'>Profiles</NavLink></li>
+            <li><NavLink to='/content'>Content</NavLink></li>
+        </ul>
         </div>
     )}
 }
